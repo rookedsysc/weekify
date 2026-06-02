80534bce989e4be1c944bfe47bbac87034c9f356
ttangta <kimjh6836@gmail.com>
2026-06-02 20:25:55 +0900

    fix: 다국어 처리 테스트 및 리뷰 코멘트 반영

--- src/main/java/com/weekify/config/SecurityConfig.java
+++ src/main/java/com/weekify/config/SecurityConfig.java
@@ -10,13 +10,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    public SecurityConfig(){
-        System.out.println("🔥SecurityConfig loaded");
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        System.out.println("🔥 SecurityFilterChain registered");
         return http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
